4fb2ff1940de994ae05183f1d9e359d48855540c
KHUSHI-KP <khushikp1102@gmail.com>
2026-09-19 22:47:01 +0530

    19-09-26

---
+++ Memorization/fib.java
@@ -0,0 +1,35 @@
+
+public class fib {
+
+    static int fib(int n, int[] memo) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+
+        memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+
+        return memo[n];
+    }
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        int[] memo = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            memo[i] = -1;
+        }
+
+        System.out.println(fib(n, memo));
+    }
+}
